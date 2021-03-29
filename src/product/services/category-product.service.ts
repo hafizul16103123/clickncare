28ad927b416633a4f72e66f63b26c9411d5d7d9f
Ashofiq <shafiq@zaynaxgroup.com>
2021-03-29 11:05:02 +0600

    merge

--- src/product/services/category-product.service.ts
+++ src/product/services/category-product.service.ts
@@ -10,6 +10,9 @@ import { SellerCountry } from '../entities/country.entity';
 import { SellerBrand } from '../entities/brand.entity';
 import { Category } from 'src/category/entities/category.entity';
 import { CategoryService } from 'src/category/category.service';
+import { ClientGrpcProxy } from '@nestjs/microservices';
+import { Schema } from 'mongoose';
+import { Attribute_Filter } from '../entities/attribute_filter.entity';
 
 @Injectable()
 export class CategoryProductService {
@@ -30,6 +33,11 @@ export class CategoryProductService {
 
     @InjectModel(Category)
     private readonly categoryModel: ReturnModelType<typeof Category>,
+
+    @InjectModel(Attribute_Filter)
+    private readonly categoryLeftFilter: ReturnModelType<
+      typeof Attribute_Filter
+    >,
 
     private readonly categoryService: CategoryService,
   ) {}
@@ -1095,6 +1103,127 @@ export class CategoryProductService {
   private getNextPage(data: boolean, total: number, pageNum: number): number {
     if (data) return null;
     return Math.ceil(total / config.pageLimit) === pageNum ? null : pageNum + 1;
+  }
+
+  async getCategoryLeftFilter(categoryId: number, data: any): Promise<any> {
+    //console.log(data.data[0]);
+    const getCategoryLeftFilter = await this.categoryLeftFilter.find({
+      categoryId: categoryId,
+    });
+
+    // console.log(getCategoryLeftFilter);
+    // console.log(data.data);
+    const filteredAttr = [];
+
+    for (const item of getCategoryLeftFilter) {
+      let matchCount = 0;
+      let totalCount = Object.keys(data).length;
+      for (const [userKey, userValue] of Object.entries<string>(data)) {
+        if (item.data[userKey]) {
+          if (
+            item.data[userKey].toLowerCase().trim() ===
+            userValue.toLowerCase().trim()
+          ) {
+            matchCount++;
+          }
+        }
+      }
+      if (matchCount === totalCount) {
+        filteredAttr.push(item);
+      }
+    }
+
+    //console.log(filteredAttr)
+
+    const categoryAttr = [];
+    let attrObj = {};
+    for (const item of filteredAttr) {
+      for (const [key, value] of Object.entries(item.data)) {
+        const attr_low = key.replace(/_/g, ' ');
+        const attr_cap = attr_low.replace(/(^\w{1})|(\s+\w{1})/g, (letter) =>
+          letter.toUpperCase(),
+        );
+
+        attrObj = {
+          categoryId: item.categoryId,
+          attributeName: key,
+          attributeLabel: attr_cap,
+          attributeValue: value,
+        };
+
+        categoryAttr.push(attrObj);
+      }
+    }
+
+    const results = [];
+
+    for (const item of categoryAttr) {
+      const resItem = results.findIndex(
+        (e) =>
+          e.categoryId === item.categoryId &&
+          e.attributeName === item.attributeName,
+      );
+      if (resItem !== -1) {
+        results[resItem].attributeValue = Array.from(
+          new Set([...results[resItem].attributeValue, item.attributeValue]),
+        );
+      } else {
+        results.push({ ...item, attributeValue: [item.attributeValue] });
+      }
+    }
+
+    const f_res = results.map((e) => {
+      const attr = e.attributeValue.map((m) => {
+        return {
+          title: m,
+          value: m,
+        };
+      });
+
+      return {
+        categoryId: e.categoryId,
+        attributeName: e.attributeName,
+        attributeLabel: e.attributeLabel,
+        attributeValue: attr,
+      };
+    });
+
+    return f_res;
+  }
+
+  async createAttribute(
+    categoryId: number,
+    sellerId: string,
+    data: any,
+  ): Promise<any> {
+    //console.log(data.data[0]);
+    //const storePro = await this.productInfo.find();
+
+    const attrData = {};
+    for (const item of data) {
+      const userKey = item.key;
+      const userValue = item.value;
+
+      if (userKey) {
+        attrData[userKey] = userValue;
+      }
+    }
+
+    const obj = {
+      categoryId: categoryId,
+      sellerId: sellerId,
+      data: attrData,
+    };
+
+    const isExist = await this.categoryLeftFilter.findOne({
+      categoryId: categoryId,
+      sellerId: sellerId,
+      data,
+    });
+
+    if (isExist === null) {
+      const seller_attr_save_result = await this.categoryLeftFilter.create(obj);
+    }
   }
 
   async subCategory(category) {
